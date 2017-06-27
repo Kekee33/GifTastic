487311050b83fb47e55c564f8217473507ac887f
keisha <keisha@sample.com>
2017-06-27 14:09:41 -0400

    added functions

--- javascript/app.js
+++ javascript/app.js
@@ -6,14 +6,54 @@ var topics = ["Bugs Bunny", "Mickey Mouse", "Simba", "Pokemon", "Gumball", "Clar
       
     function displayToons() {
 
-    var movie = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC";
+    var toon = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=cartoons&api_key=dc6zaTOxFJmzC&limit=5&rating=pg";
 
     $.ajax({
     url: queryURL,
     method: "GET"
     }).done(function(response) {
-    $("#movies-view").html(JSON.stringify(response));
-    renderButtons();
+    console.log(response);
+    console.log(response.data[0].url);    
+    $("#toon-view").append(response.data[0].url);  
+    
+    renderButtons();  
+  
     });
     }
+
+    function renderButtons() {
+
+     $("#buttons-view").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+
+          var a = $("<button>");
+          
+          a.addClass("toon");
+          
+          a.attr("data-name", topics[i]);
+          
+          a.text(topics[i]);
+          
+          $("#buttons-view").append(a);
+        }
+      }
+
+      $("#add-toon").on("click", function(event) {
+        event.preventDefault();
+
+        var toons = $("#toon-input").val().trim();
+
+        
+        topics.push(toons);
+        console.log(topics)
+
+        
+        renderButtons();
+      });
+
+      $(document).on("click", ".toon", displayToons);
+
+      
+      renderButtons();
